d8a4ad3b82407eaa4009bc0db8ebead5ed7449f1
Salameroo <gabriels51102@gmail.com>
2024-06-17 18:37:09 +0200

    Funciona plisss

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,18 +1,26 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class Cors
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            //Url a la que se le dará acceso en las peticiones
-            ->header("Access-Control-Allow-Origin", [env('FRONTEND_URL', 'https://www.cargram.asalamero.dawmor.cloud/'), 'https://www.cargram.asalamero.dawmor.cloud/'])
-            //Métodos que a los que se da acceso
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-            //Headers de la petición
-            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', 'https://www.cargram.asalamero.dawmor.cloud');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+
+        if ($request->getMethod() == "OPTIONS") {
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            $response->setStatusCode(200);
+        }
+
+        return $response;
     }
 }
