c978de69172b26b5a3e9c41417740f473993145e
Salameroo <gabriels51102@gmail.com>
2024-06-17 18:49:03 +0200

    Prueba

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,22 +3,22 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class Cors
 {
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        // Allow all origins, methods, and headers for testing purposes
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        // Allow specific origin
+        $response->headers->set('Access-Control-Allow-Origin', 'https://www.cargram.asalamero.dawmor.cloud');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         if ($request->getMethod() === 'OPTIONS') {
             return response('', 204)->withHeaders([
-                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Origin' => 'https://www.cargram.asalamero.dawmor.cloud',
                 'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
                 'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
                 'Access-Control-Allow-Credentials' => 'true',
