206a0b90e498eb0b3c6a55c86ad3452c296bac3c
Salameroo <gabriels51102@gmail.com>
2024-06-18 00:35:34 +0200

    Quiero volver atrasporfa

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,28 +1,18 @@
 <?php
 
+// app/Http/Middleware/Cors.php
 namespace App\Http\Middleware;
 
 use Closure;
 
 class Cors
 {
-    protected $allowedOrigins = [
-        'https://www.cargram.asalamero.dawmor.cloud/',
-        'https://www.cargram.asalamero.dawmor.cloud'
-    ];
-
     public function handle($request, Closure $next)
     {
-        $origin = $request->headers->get('origin');
-
-        if (in_array($origin, $this->allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-Token')
-                ->header('Access-Control-Allow-Credentials', 'true');
-        }
-
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://www.cargram.asalamero.dawmor.cloud')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -2,13 +2,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://www.cargram.asalamero.dawmor.cloud/',
-        'https://www.cargram.asalamero.dawmor.cloud'
-    ],
+    'allowed_origins' => ['https://www.cargram.asalamero.dawmor.cloud'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Set to false if you want to disable credentials support
+    'supports_credentials' => true,
 ];
