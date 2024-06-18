59cfe37243666c4dcee186bcef5f744830f71a63
Salameroo <gabriels51102@gmail.com>
2024-06-18 08:45:14 +0200

    P^saoujd

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://www.cargram.asalamero.dawmor.cloud'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    'allowed_origins' => ['https://www.cargram.asalamero.dawmor.cloud'], // Tu dominio frontend
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'Origin'], // Headers permitidos
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => true, // Asegúrate de que esto esté en true
 ];
