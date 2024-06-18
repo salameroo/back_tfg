4ab5be29425ebddf1beaadc29b6aeeff92082be2
Salameroo <gabriels51102@gmail.com>
2024-06-18 09:24:49 +0200

    PRuebas

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,8 +9,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Origin', 'https://www.cargram.asalamero.dawmor.cloud')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Credentials', true)
             ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
             ->header('Accept', 'application/json');
